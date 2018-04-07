01a8e6ebefb1e5956e79963c9adc045ae4c1ccc1
b <biro123@ntlworld.com>
2018-04-07 20:35:31 +0100

    Add custom inspector for waypointNetwork

--- Assets/Editor/AIWaypointNetworkEditor.cs
+++ Assets/Editor/AIWaypointNetworkEditor.cs
@@ -2,9 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEngine.AI;
 
 [CustomEditor(typeof(AIWaypointNetwork))]
 public class AIWaypointNetworkEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        // target is the reference to the opject which the editor script relates to
+        // We need to CAST it as its type (AIWaypointNetwork)
+        AIWaypointNetwork waypointNetwork = (AIWaypointNetwork)target;
+
+        waypointNetwork.displayMode
+            = (PathDisplayMode)EditorGUILayout.EnumPopup("Display Mode", waypointNetwork.displayMode);
+
+        if (waypointNetwork.displayMode == PathDisplayMode.Paths)
+        {
+            waypointNetwork.UIStart = EditorGUILayout.IntSlider
+                ("Waypoint Start", waypointNetwork.UIStart, 0, waypointNetwork.waypoints.Count - 1);
+            waypointNetwork.UIEnd = EditorGUILayout.IntSlider
+                ("Waypoint End", waypointNetwork.UIEnd, 0, waypointNetwork.waypoints.Count - 1);
+        }
+
+        DrawDefaultInspector();
+    }
 
     private void OnSceneGUI()
     {
@@ -23,7 +44,19 @@ public class AIWaypointNetworkEditor : Editor {
             }
         }
 
+        if (waypointNetwork.displayMode == PathDisplayMode.Connections)
+        {
+            DisplayConnections(waypointNetwork);
+        }
+        else if (waypointNetwork.displayMode == PathDisplayMode.Paths)
+        {
+            DisplayNavmeshPath(waypointNetwork);
+        }
 
+    }
+
+    private void DisplayConnections(AIWaypointNetwork waypointNetwork)
+    {
         // Draw lines connecting waypoints
         Vector3[] linePoints = new Vector3[waypointNetwork.waypoints.Count + 1];
 
@@ -39,12 +72,28 @@ public class AIWaypointNetworkEditor : Editor {
                 linePoints[i] = waypointNetwork.waypoints[index].position;
             }
             else  // Make empty array item obvious 
-            {                
+            {
                 linePoints[i] = new Vector3(Mathf.Infinity, Mathf.Infinity, Mathf.Infinity);
-            }            
+            }
         }
         Handles.color = Color.cyan;
         Handles.DrawPolyLine(linePoints);
     }
 
+    private void DisplayNavmeshPath(AIWaypointNetwork waypointNetwork)
+    {
+        NavMeshPath path = new NavMeshPath();
+
+        if (waypointNetwork.waypoints[waypointNetwork.UIStart] != null &&
+           waypointNetwork.waypoints[waypointNetwork.UIEnd] != null)
+        {
+            Vector3 from = waypointNetwork.waypoints[waypointNetwork.UIStart].position;
+            Vector3 to = waypointNetwork.waypoints[waypointNetwork.UIEnd].position;
+
+            NavMesh.CalculatePath(from, to, NavMesh.AllAreas, path);
+            // draw line connecting all points on the returned path
+            Handles.color = Color.yellow;
+            Handles.DrawPolyLine(path.corners);
+        }
+    }
 }
--- Assets/_Scripts/AIWaypointNetwork.cs
+++ Assets/_Scripts/AIWaypointNetwork.cs
@@ -2,6 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PathDisplayMode {None, Connections, Paths }
+
 public class AIWaypointNetwork : MonoBehaviour {
+
+    // HideInInspector so we can render them in our own custome editor instead 
+    [HideInInspector]
+    public PathDisplayMode displayMode = PathDisplayMode.Connections;
+    [HideInInspector]
+    public int UIStart = 0;
+    [HideInInspector]
+    public int UIEnd = 0;
+
     public List<Transform> waypoints = new List<Transform>();
 }
